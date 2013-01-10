b838ba50bc62cbdad5fc51eaccac9248393b83a4
Chetan Sarva <chetan@pixelcop.net>
2013-01-10 18:18:56 -0500

    use newer ruby version

--- config/projects/bixby.rb
+++ config/projects/bixby.rb
@@ -1,8 +1,8 @@
 
 name "bixby"
 
-install_path 	"/opt/bixby"
-build_version	Omnibus::BuildVersion.new.semver
-build_iteration		"1"
+install_path 	    "/opt/bixby"
+build_version	    Omnibus::BuildVersion.new.git_describe # semver
+build_iteration	  "1"
 
-dependencies %w{ preparation }
+dependencies      %w{ preparation ruby rubygems bundler }

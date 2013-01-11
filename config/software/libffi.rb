30fb35e7112c8e7179d78f3ea496ec8b69052624
Chetan Sarva <chetan@pixelcop.net>
2013-01-11 00:39:51 -0500

    libffi package

---
+++ config/software/libffi.rb
@@ -0,0 +1,23 @@
+
+name "libffi"
+version "3.0.11"
+md5 = "f69b9693227d976835b4857b1ba7d0e3"
+
+dependencies [ ]
+
+source :url => "ftp://sourceware.org/pub/libffi/libffi-#{version}.tar.gz",
+       :md5 => md5
+
+relative_path "libpng-#{version}"
+
+configure_env = {
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+}
+
+build do
+  command "./configure --prefix=#{install_dir}/embedded", :env => configure_env
+  command "make -j #{max_build_jobs}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
+  command "make install"
+end

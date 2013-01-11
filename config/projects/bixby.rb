bb1ec140e01814fa260a45cebb5cd6ac5f0ef80d
Chetan Sarva <chetan@pixelcop.net>
2013-01-10 20:15:24 -0500

    added version manifest

--- config/projects/bixby.rb
+++ config/projects/bixby.rb
@@ -5,4 +5,4 @@ install_path 	    "/opt/bixby"
 build_version	    Omnibus::BuildVersion.new.git_describe # semver
 build_iteration	  "1"
 
-dependencies      %w{ preparation ruby rubygems bundler }
+dependencies      %w{ preparation ruby rubygems bundler version-manifest }

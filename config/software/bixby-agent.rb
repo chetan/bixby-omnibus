69dcb33e0c07043d635ae5c64cbb98700d377ba5
Chetan Sarva <chetan@pixelcop.net>
2013-01-10 23:26:16 -0500

    first pass at bixby-agent package

---
+++ config/software/bixby-agent.rb
@@ -0,0 +1,66 @@
+
+name "bixby-agent"
+version ENV["BIXBY_GIT_REV"] || "master"
+always_build true
+
+dependencies %w{ rubygems }
+
+source :git => "https://github.com/chetan/bixby-agent.git"
+
+# setup ENV for compilation
+env =
+  case platform
+  when "solaris2"
+    if Omnibus.config.solaris_compiler == "studio"
+    {
+      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+      "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
+    }
+    elsif Omnibus.config.solaris_compiler == "gcc"
+    {
+      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+      "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
+      "LD_OPTIONS" => "-R#{install_dir}/embedded/lib"
+    }
+    else
+      raise "Sorry, #{Omnibus.config.solaris_compiler} is not a valid compiler selection."
+    end
+  else
+    {
+      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+      "LDFLAGS" => "-Wl,-rpath #{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
+    }
+  end
+
+build do
+
+  bundle "install", :env => env
+  rake "build", :env => env
+
+  cmd = cmd_str <<-EOF
+    install pkg/bixby-agent-*.gem
+    -n #{install_dir}/bin
+    --no-rdoc --no-ri
+    EOF
+
+  gem cmd, :env => env
+
+  #
+  # TODO: the "clean up" section below was cargo-culted from the
+  # clojure version of omnibus that depended on the build order of the
+  # tasks and not dependencies. if we really need to clean stuff up,
+  # we should probably stick the clean up steps somewhere else
+  #
+
+  # clean up
+  ["docs",
+   "share/man",
+   "share/doc",
+   "share/gtk-doc",
+   "ssl/man",
+   "man",
+   "info"].each do |dir|
+    command "rm -rf #{install_dir}/embedded/#{dir}"
+  end
+
+end

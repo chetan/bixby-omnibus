07cbcc341e878fa7df8caf39e8f25891d65ce30d
Chetan Sarva <chetan@pixelcop.net>
2013-01-10 22:55:04 -0500

    cleanup

--- config/software/oj.rb
+++ config/software/oj.rb
@@ -1,14 +1,13 @@
 
 name "oj"
-gem_version = Bixby.gem_version(name)
-version gem_version
+version Bixby.gem_version(name)
 
-dependencies ["rubygems"]
+dependencies %w{ rubygems }
 
 build do
   gem cmd_str <<-EOF
-    install oj
-      -v #{gem_version}
+    install #{name}
+      -v #{version}
       -n #{install_dir}/bin
       --no-rdoc --no-ri
   EOF

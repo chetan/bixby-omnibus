2290712914545f587f1541bbcd1de945ddccc594
Chetan Sarva <chetan@pixelcop.net>
2013-02-11 17:00:21 -0500

    install bixby-client gem from git

---
+++ config/software/bixby-client.rb
@@ -0,0 +1,31 @@
+
+name "bixby-client"
+version "LAST_TAG"
+
+dependencies %w{ rubygems bundler }
+
+source :git => "https://github.com/chetan/api_auth.git"
+
+# setup ENV for compilation
+env = {
+  "CFLAGS"  =>     "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LDFLAGS" => "-Wl,-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
+}
+
+build do
+
+  # install all runtime deps into install_dir
+  bundle "install --without development test", :env => env
+
+  # install all deps (including dev and test) into vendor for building gem
+  bundle "install --deployment --without ''", :env => env
+  rake "build", :env => env
+
+  cmd = cmd_str <<-EOF
+    install pkg/*.gem
+    --no-rdoc --no-ri
+    EOF
+
+  gem cmd, :env => env
+
+end

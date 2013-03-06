9a4254c3399ac554f0e4c384d8fb458c43cb14ef
Chetan Sarva <chetan@pixelcop.net>
2013-03-05 19:00:06 -0500

    fix repo build

--- config/software/bixby-repo.rb
+++ config/software/bixby-repo.rb
@@ -7,14 +7,15 @@ source :git => "https://github.com/chetan/bixby-repo.git"
 build do
 
   block do
-    system("cp -a #{project_dir} #{install_dir}/repo")
-
     # remove files we don't need
     %w{.git .test .gitignore Gemfile Gemfile.lock Rakefile README
        update_digests.sh}.each do |file|
 
-      system("rm -rf #{install_dir}/repo/#{file}")
+      system("rm -rf #{project_dir}/repo/#{file}")
     end
+
+    system("mkdir #{install_dir}/repo")
+    system("cp -a #{project_dir} #{install_dir}/repo/vendor")
   end
 
 end

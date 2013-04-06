748bcfaabc8fdba7a00442f8bc92e4fcf746631f
Chetan Sarva <chetan@pixelcop.net>
2013-04-06 12:15:38 -0400

    docs

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# run's the actual build and creates a package via fpm
+# run's the actual build and creates a package via fpm.
+#
+# this should be run inside a vm on the target platform.
+# normally called from bootstrap.sh
 
 export ROOT=$(dirname $(readlink -f $0))
 cd $ROOT

2caaf328c83faca7412b884adfd194ba19de3a61
Chetan Sarva <chetan@pixelcop.net>
2013-08-26 15:21:41 -0400

    remove rakefile; require libs from project

--- config/projects/bixby.rb
+++ config/projects/bixby.rb
@@ -8,3 +8,8 @@ build_version     "placeholder" # will be set in bixby-agent package
 build_iteration   1
 
 dependencies      %w{ preparation ruby rubygems bundler bixby-agent version-manifest }
+
+OMNIBUS_ROOT = File.expand_path("../..", __FILE__)
+require File.join(OMNIBUS_ROOT, "lib/patch_omnibus_http")
+require File.join(OMNIBUS_ROOT, "lib/patch_omnibus_build_gem")
+require File.join(OMNIBUS_ROOT, "lib/gem_version")

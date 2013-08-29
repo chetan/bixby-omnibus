0dbb5bd9196623e4d7c3ed7210671bfea2893b67
Chetan Sarva <chetan@pixelcop.net>
2013-08-29 00:35:56 -0400

    cleanup

--- config/projects/bixby.rb
+++ config/projects/bixby.rb
@@ -9,7 +9,3 @@ build_version     "placeholder" # will be set in bixby-agent package
 build_iteration   1
 
 dependencies      %w{ preparation ruby rubygems bundler bixby-agent version-manifest }
-
-# require File.join(Omnibus::Config.project_root, "lib/patch_omnibus_http")
-# require File.join(Omnibus::Config.project_root, "lib/patch_omnibus_build_gem")
-# require File.join(Omnibus::Config.project_root, "lib/gem_version")

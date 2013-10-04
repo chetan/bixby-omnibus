664b8ed6d05365e616d8759275a2ba5cd75ab1b6
Chetan Sarva <chetan@pixelcop.net>
2013-10-04 17:40:23 -0400

    send stderr to stdout so we can log it

--- scripts/shim.sh
+++ scripts/shim.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-\wget -q --no-check-certificate https://raw.github.com/chetan/bixby-omnibus/master/scripts/bootstrap.sh -O - | /bin/bash | tee $HOME/bixby-omnibus.log
+\wget -q --no-check-certificate https://raw.github.com/chetan/bixby-omnibus/master/scripts/bootstrap.sh -O - | /bin/bash 2>&1 | tee $HOME/bixby-omnibus.log

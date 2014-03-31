607fee926f61f0917de720d4eb5a04af5210cbc3
Chetan Sarva <chetan@pixelcop.net>
2014-03-31 16:58:24 -0400

    another rvm fix

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -171,7 +171,7 @@ fi
 if [[ -z `which ruby 2>/dev/null` || ! `ruby -v | grep 1.9.3` ]]; then
   cd
   \curl -sSL https://get.rvm.io | sudo PATH="$PATH:/usr/sbin" bash -s stable
-  sudo /usr/sbin/usermod -a -G rvm vagrant
+  sudo /usr/sbin/usermod -a -G rvm $USER
   newgrp rvm
 
   # load rvm
@@ -184,7 +184,7 @@ if [[ -z `which ruby 2>/dev/null` || ! `ruby -v | grep 1.9.3` ]]; then
     # use our binaries for centos 5 or 6
     echo "rvm_remote_server_url3=https://s3.amazonaws.com/s3.bixby.io/rubies" | cat /usr/local/rvm/user/db - | sudo tee /usr/local/rvm/user/db >/dev/null
   fi
-  command rvm install 1.9.3-p484
+  rvmsudo rvm install 1.9.3-p484
   rvm use 1.9.3-p484 --default
   # source /usr/local/rvm/environments/ruby-1.9.3-p484
 fi

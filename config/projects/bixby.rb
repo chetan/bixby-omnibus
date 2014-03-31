944d12bc62a88152c825bf82b8a7da1ad27fe0c1
Chetan Sarva <chetan@pixelcop.net>
2014-03-31 09:02:54 -0400

    require ruby 2.1.1

--- config/projects/bixby.rb
+++ config/projects/bixby.rb
@@ -8,4 +8,6 @@ install_path      "/opt/bixby"
 build_version     "placeholder" # will be set in bixby-agent package
 build_iteration   1
 
+override :ruby, :version => "2.1.1"
+
 dependencies      %w{ preparation libffi ruby rubygems rbnacl bundler bixby-agent version-manifest }

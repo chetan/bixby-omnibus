ecc4791feca0456a34fc420791d45d25749ee253
Chetan Sarva <chetan@pixelcop.net>
2014-03-31 15:48:31 -0400

    use nohup to run bootstrap in bg

--- scripts/shim.sh
+++ scripts/shim.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
-\wget -q --no-check-certificate https://raw.github.com/chetan/bixby-omnibus/master/scripts/bootstrap.sh -O - | /bin/bash 2>&1 | tee $HOME/bixby-omnibus.log
+run_build() {
+  \wget -q --no-check-certificate https://raw.github.com/chetan/bixby-omnibus/master/scripts/bootstrap.sh -O - | /bin/bash 2>&1 | tee $HOME/bixby-omnibus.log
+}
+
+nohup run_build >/dev/null 2>nohup.err </dev/null &

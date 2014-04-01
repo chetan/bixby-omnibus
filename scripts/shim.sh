a5937c16cea628256069c8253c1c8bd1bbf28ed0
Chetan Sarva <chetan@pixelcop.net>
2014-04-01 17:46:19 -0400

    always write script

--- scripts/shim.sh
+++ scripts/shim.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
-if [ ! -f go.sh ]; then
-  echo "cd bixby-omnibus && git pull -q && scripts/build.sh 2>&1 | tee $HOME/bixby-omnibus.log" > go.sh
-fi
+rm -f go.sh
+echo "cd bixby-omnibus && git pull -q && scripts/build.sh 2>&1 | tee $HOME/bixby-omnibus.log" > go.sh
 
 nohup /bin/bash go.sh >/dev/null 2>nohup.err </dev/null &

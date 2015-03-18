7c885e8b8d4fcb471791c48b3a9618ff1b778097
Chetan Sarva <chetan@pixelcop.net>
2015-03-18 13:13:01 -0400

    rename log to build.log

--- scripts/shim.sh
+++ scripts/shim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 rm -f go.sh
-echo "cd bixby-omnibus && git pull -q && scripts/build.sh 2>&1 | tee $HOME/bixby-omnibus.log" > go.sh
+echo "cd bixby-omnibus && git pull -q && scripts/build.sh 2>&1 | tee $HOME/build.log" > go.sh
 
 nohup /bin/bash go.sh >/dev/null 2>nohup.err </dev/null &

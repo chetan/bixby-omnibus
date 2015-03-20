86257624d492944da6b324b925e8dac5758a7cf5
Chetan Sarva <chetan@pixelcop.net>
2015-03-20 11:05:47 -0400

    added help for shim.sh

--- scripts/shim.sh
+++ scripts/shim.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -e
 
+# kicks off the build process, normally from bin/build util
+#
+# usage:
+# \wget -q --no-check-certificate https://raw.github.com/chetan/bixby-omnibus/master/scripts/shim.sh -O - | /bin/bash -s
+#
+# optional ENV vars to pass to bash:
+# BIXBY_GIT_REV=<sha>         build a specific revision
+# CLEAN=1                     tell omnibus to do a clean build
+
 rm -f go.sh
 echo "cd bixby-omnibus && git pull -q && scripts/build.sh 2>&1 | tee $HOME/build.log" > go.sh
 
